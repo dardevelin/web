7aad83685a127369e558ea6be91905f0671a8d8b
Josep Jaume <josepjaume@gmail.com>
2011-11-16 19:52:03 +0100

    Re-activate client category

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,11 +16,10 @@ class ProjectsController < ApplicationController
 
   def validate_category!
     raise ActiveRecord::RecordNotFound unless WorkCategory.names.include?(params[:work_category_id])
-    raise ActiveRecord::RecordNotFound if params[:work_category_id] == 'client'
   end
 
   def load_categories
-    @categories = WorkCategory.all[1..2]
+    @categories = WorkCategory.all
     @active = params[:work_category_id]
   end
 
