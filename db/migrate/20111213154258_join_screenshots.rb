fd2023092114f8cbe452758e4b84530dd946ab80
Josep Jaume Rey <josepjaume@gmail.com>
2011-12-13 18:03:24 +0100

    Add translations again

--- db/migrate/20111213154258_join_screenshots.rb
+++ db/migrate/20111213154258_join_screenshots.rb
@@ -3,7 +3,7 @@ class JoinScreenshots < ActiveRecord::Migration
     add_column :projects, :screenshot, :string
     Project.all.each do |project|
       file = project.screenshots.first.read_attribute(:file)
-      project.update_column(:screenshot, file)
+      project.update_column(:screenshot, file) unless file.blank?
     end
   end
 
