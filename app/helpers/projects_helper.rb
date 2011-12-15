34a993d48010700812b35cfd6f2d19ddb9dbaf2e
Josep Jaume Rey <josepjaume@gmail.com>
2011-12-15 09:42:48 +0100

    Do not show screenshot fallback

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,15 +2,11 @@ module ProjectsHelper
   def big_screenshot_for(project)
     if project.screenshot?
       image_tag project.screenshot.big.url
-    else
-      image_tag 'fallback/big_screenshot.jpg'
     end
   end
   def thumb_screenshot_for(project)
     if project.screenshot?
       image_tag project.screenshot.thumb.url
-    else
-      image_tag 'fallback/thumb_screenshot.jpg'
     end
   end
 end
