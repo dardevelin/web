22b36eba7e34d56ffc03c358a1cef7c959099758
Josep Jaume Rey <josepjaume@gmail.com>
2011-12-16 11:04:53 +0100

    Improve project ordering

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,12 @@ class ProjectsController < ApplicationController
   before_filter :load_categories
 
   def index
-    @projects = Project.where(category: params[:work_category_id]).ordered.all
+    @projects = Project.where(category: params[:work_category_id])
+    if params[:work_category_id] == 'open-source'
+      @projects = @projects.by_downloads
+    else
+      @projects = @projects.alphabetical
+    end
     expires_in 3.hours, 'max-stale' => 5.hours, :public => true if Rails.env.production?
     respond_to do |format|
       format.html
