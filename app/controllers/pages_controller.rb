b5ff643050c336b4d36375f08f3ddd6cd9eb9924
Josep Jaume <josepjaume@gmail.com>
2012-02-02 11:40:19 +0100

    Make cache less aggressive

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   respond_to :js, :html
 
   def show
-    expires_in 3.hours, 'max-stale' => 5.hours, :public => true if Rails.env.production?
+    expires_in 5.minutes, 'max-stale' => 5.hours, :public => true if Rails.env.production?
     respond_to do |format|
       format.html{
         render template: "pages/#{params[:id]}"
